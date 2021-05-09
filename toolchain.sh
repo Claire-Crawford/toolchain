0595de361cb48e3077dea01bc1ee5f8fa7a35e1d
David Skinner <skinner.david@gmail.com>
2021-05-09 13:25:45 -0500

    Create toolchain.sh

---
+++ toolchain.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# 'toolchain.sh'
+
+# by David Skinner
+# on May 8, 2021
+# for Davsk Ltd Co
+
+# Copyright (c) 2021 Davsk Ltd Co. All Rights Reserved
+
+# This script installs the Davsk Toolchain on Ubuntu 21.04
+
+# Updates
+sudo apt-get update
+sudo apt-get -y upgrade
+
+# linuxbrew
+sudo apt-get install build-essential procps curl file git
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# shellcheck disable=SC2016
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/david/.profile
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+brew install gcc
+brew install gh
+brew install sass/sass/sass
+
+# chrome
+sudo apt-get install gdebi-core wget
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo gdebi google-chrome-stable_current_amd64.deb
+
+# tools
+sudo apt-get install zsh go gitkraken julia cmake umbrello
+sudo apt-get install hugo goland emacs
+sudo apt-get install gimp inkscape cherrytree 
+
+# ssh
+read -rp "Enter your email: " email
+ssh-keygen -t ed25519 -C "$email"
+eval 'ssh-agent -s'
+ssh-add ~/.ssh/id_ed25519
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "~/.ssh/id_ed25519 needs to be added to GitHub.com account"
